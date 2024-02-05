ddc122ae775ad3134f975d7252c50e5f550ba61b
Alexandr <romanenyasasha@gmail.com>
2024-02-05 18:30:32 +0300

    Updated method

--- filter/delete.go
+++ filter/delete.go
@@ -1,9 +1,9 @@
 package filter
 
-func RemoveItemsFromListByCondition[T any](l *[]T, checkRemoving func(i int) bool) {
+func RemoveItemsFromListByCondition[T any](l *[]T, checkRemoving func(item T) bool) {
 	newList := make([]T, 0)
-	for i, item := range *l {
-		if checkRemoving(i) {
+	for _, item := range *l {
+		if checkRemoving(item) {
 			continue
 		}
 		newList = append(newList, item)
