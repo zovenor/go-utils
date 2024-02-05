4e8164d78b61b19743c80484995a9c69e68890c2
Alexandr <romanenyasasha@gmail.com>
2024-02-05 18:59:06 +0300

    Added error

--- filter/delete.go
+++ filter/delete.go
@@ -1,12 +1,21 @@
 package filter
 
-func RemoveItemsFromListByCondition[T any](l *[]T, checkRemoving func(item T) bool) {
+import "fmt"
+
+func RemoveItemsFromListByCondition[T any](l *[]T, checkRemoving func(item T) bool) error {
 	newList := make([]T, 0)
+	f := false
 	for _, item := range *l {
 		if checkRemoving(item) {
 			continue
 		}
 		newList = append(newList, item)
+		f = true
 	}
 	*l = newList
+	if !f {
+		return fmt.Errorf("items not found")
+	} else {
+		return nil
+	}
 }
